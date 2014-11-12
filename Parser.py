1fa3f1d48ad5b9789e87229de8832f435dc142ec
hning <hning@umich.edu>
2014-11-12 10:51:59 -0500

    prototype working, need to fix elements that are split

--- Parser.py
+++ Parser.py
@@ -5,25 +5,50 @@ from pdfminer.converter import TextConverter, PDFPageAggregator
 from pdfminer.layout import LAParams, LTTextBox, LTTextLine, LTFigure, LTImage, LTTextLineHorizontal, LTTextBoxHorizontal, \
                             LTChar, LTRect, LTLine
 from pdfminer.pdfpage import PDFPage
-from operator import itemgetter
+from operator import itemgetter, attrgetter, methodcaller
 # from cStringIO import StringIO
 import os
 import base64, re, datetime, StringIO
 
-threshold = 0.5
+overlap_threshold = 0.5
+
+def strip_text(input):
+    return input.replace('\n','').lstrip().rstrip();
 
 class TableRow:
     def __init__(self, element):
-        self.data = []
+        self.data = [element]
         self.min_y = element.y0
         self.max_y = element.y1
+        self.max_size = self.max_y - self.min_y
 
     def __getitem__(self, index):
         return self.data[index]
 
+    def __str__(self):
+        output = "|"
+        for d in self.data:
+            #print strip_text(d.get_text())
+            text = strip_text(d.get_text())
+            output += text.encode('utf-8');
+            output += "|".format()
+        return output
+
+    def __len__(self):
+        return len(self.data)
+        
     def add(self, obj):
-        print "hi"
-        #if object.y0 < row.min_y 
+        obj_size = obj.y1 - obj.y0
+        if obj_size > self.max_size:
+            self.min_y = obj.y0
+            self.max_y = obj.y1
+            self.max_size = obj_size
+
+        self.data.append(obj);
+
+    def sort(self):
+        self.data = sorted(self.data,
+            key=attrgetter('x0'))
 
 
 class TableRows:
@@ -33,11 +58,23 @@ class TableRows:
     def __getitem__(self, index):
         return self.rows[index]
 
+    def __str__(self):
+        output = ""
+        for row in self.rows:
+            output += str(row)
+            output += "\n"
+        return output
+
+    def __len__(self):
+        return len(self.rows)
+
     def add(self, obj):
-        for row in rows:
+        #print obj
+        #print strip_text(obj.get_text()).encode('utf-8')
+        for row in self.rows:
             #Use set intersection
-            newR = range(round(obj.y0), round(obj.y1))
-            rowR = range(round(row.min_y), round(row.max_y))
+            newR = range(int(round(obj.y0)), int(round(obj.y1)))
+            rowR = range(int(round(row.min_y)), int(round(row.max_y)))
 
             newS = set(newR)
             rowS = set(rowR)
@@ -49,33 +86,54 @@ class TableRows:
             else:
                 minSize = obj.y1 - obj.y0
 
-            if len(resultIntersect) >= (minSize * threshold):
+            if len(resultIntersect) >= (minSize * overlap_threshold):
                 #Found row
                 row.add(obj)
                 return
 
+        self.rows.append(TableRow(obj))
+
+    #TODO: Finish this method
+    def merge_rows(self):
+        self.rows = sorted(self.rows, key=attrgetter('max_size'))
+        for i in range(0,len(self.rows)):
 
 
+    def sort(self):
+        #Need to merge elements before sort
+        self.merge_rows()
+        
+        for row in self.rows:
+            row.sort()
+        self.rows = sorted(self.rows,
+            key=attrgetter('min_y'), reverse=True)
 
 
-def FindLines(layout):
+def getRows(layout):
     objstack = list(reversed(layout._objs))
-    xlines = [ ]
-    ylines = [ ]
+    rows = TableRows()
     while objstack:
         obj = objstack.pop()
 
        
         if type(obj) in [LTTextBoxHorizontal]:
-            text = obj.get_text().replace('\n', '')
-            text = text.lstrip().rstrip();
+            # text = obj.get_text().replace('\n', '')
+            # text = text.lstrip().rstrip();
 
+            text = strip_text(obj.get_text())
             if len(text) > 0:
-                print text.encode('utf-8');
-                print "{0} {1} {2} {3}".format(obj.x0, obj.x1,
-                    obj.y0,obj.y1)
-                print obj
-                print ""
+                # print text.encode('utf-8');
+                # print "{0} {1} {2} {3}".format(obj.x0, obj.x1,
+                #     obj.y0,obj.y1)
+                # print obj
+                # print ""
+
+                rows.add(obj)
+
+    rows.sort()
+    print ""
+    print rows
+
 
     #TODO Add code to determine which boxes are in the same row
     #Strategy: Keep a 2D array of objects that hold lists of elements for each row
@@ -126,8 +184,8 @@ def convert_pdf_to_txt(path):
         password=password,caching=caching, check_extractable=True):
         interpreter.process_page(page)
         layout = device.get_result()
-        FindLines(layout)
-        print layout
+        getRows(layout)
+       # print layout
 
 
     return str
